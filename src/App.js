dd76b7da9b8bf87fbc0573780d7e19c2926d8340
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-01-26 23:59:01 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar'
 
 
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar/>
+        <Switch>
+          <Route path='/home' exact component={Home}/>
+          <Route path='/service' exact component={Service}/>
+          <Route path='/consulting' exact component={Consulting}/>
+          <Route path='/contactus' exact component={ContactUs}/>
+          <Route path='/product' exact component={Product}/>
+          <Route path='/marketing' exact component={Marketing}/>
+          <Route path='/signup' exact component={SignUp}/>
+        </Switch>
       </Router>
       
     </div>
